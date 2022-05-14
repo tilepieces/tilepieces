f068f812a97a9a0c71ddccda5ffd8b36eef6b4f7
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-05-14 12:17:48 +0200

    version 0.1.16
    - in HTML inspector panel, changed scroll parameters for a bug on IOS.
    - fixed a bug when concatenating sources

--- modules/tilepieces/core/tilepieces-core.js
+++ modules/tilepieces/core/tilepieces-core.js
@@ -140,7 +140,7 @@ document.body.append(highlightOver, selectionDiv, paddingDiv, marginDiv, borderD
 
 let drawSelection;//requestAnimationFrame reference
 window.tilepieces = {
-  version : "0.1.15",
+  version : "0.1.16",
   projects: [],
   globalComponents: [],
   localComponents: [],
